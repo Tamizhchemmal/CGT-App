d0816f772fc54063641d0c0b022c49f8c7661008
Tamizhchemmal <tamizhchemmal@gmail.com>
2023-07-12 14:42:33 +0530

    first push

---
+++ cgt/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Adminlogin from './Components/Adminlogin';
+
+function App() {
+  return (
+   
+     <>
+     <Adminlogin/>
+     </>
+  );
+}
+
+export default App;
---
+++ cgt/src/Components/Adminlogin.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react'
+import '../Css/login.css'
+
+import {TextField,Button} from '@mui/material'
+function Adminlogin() {
+    const [email,setEmail]= useState('');
+    const[password,setPassword] = useState('');
+    const[error,setError]=useState('')
+
+  
+
+
+    const submitAdminLogin=(e)=>{
+  
+      if(email==="abc@gmail.com" && password==="12345"){
+        alert('success')
+        setError('');
+      }else if(email!=="abc@gmail.com"){
+         setError('Incorrect Email')
+         return error;
+      } else{
+        setError('incorrect password')
+        setEmail('')
+        setPassword('')
+      
+        return error ;
+
+      }
+     
+    }
+  return (
+    <>
+    <div className='main-page'>
+    <div className='login-card'>
+         <h2>Admin Login</h2>
+         <div>
+            <form>
+                <div className='inputs-admin'>
+                    
+                    <TextField id="standard-basic"  value={email} onChange={(e)=>{setEmail(e.target.value)}} type='text' sx={{width:"80%",margin:"10px 0 10px 0"}} label="Username" variant="standard" required/>
+                    
+                
+                   <TextField id="standard-basic" type='password' value={password} onChange={(e)=>{setPassword(e.target.value)}} sx={{width:"80%",margin:"10px  0 10px 0"}} label="Password" variant="standard" required/>
+                
+               {error && <div>{error}</div>}
+                    
+                 <Button type='submit' sx={{  width: "80%"}} onClick={submitAdminLogin} size='large' variant="outlined"className='btn-admin' color='secondary' >Login</Button>
+
+                </div>
+           
+
+            </form>
+          
+         </div>
+     </div> 
+    </div>
+     
+    </>
+  )
+}
+
+export default Adminlogin
