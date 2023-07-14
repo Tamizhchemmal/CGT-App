614015f1965ba47053fbe67f5adcdf9c9edb78ae
Tamizhchemmal <tamizhchemmal@gmail.com>
2023-07-14 15:59:21 +0530

    2 Pages Added

--- cgt/src/Components/Adminlogin.jsx
+++ cgt/src/Components/Adminlogin.jsx
@@ -7,9 +7,6 @@ function Adminlogin() {
     const[password,setPassword] = useState('');
     const[error,setError]=useState('')
 
-  
- const[name,setName]=useState();
-
     const submitAdminLogin=(e)=>{
   
       if(email==="abc@gmail.com" && password==="12345"){
@@ -32,9 +29,13 @@ function Adminlogin() {
     <>
     <div className='main-page'>
     <div className='login-card'>
+
+          <span>Career Guidance Technologies</span>
+           
          <h2>Admin Login</h2>
          <div>
             <form>
+         
                 <div className='inputs-admin'>
                     
                     <TextField id="standard-basic"  value={email} onChange={(e)=>{setEmail(e.target.value)}} type='text' sx={{width:"80%",margin:"10px 0 10px 0"}} label="Username" variant="standard" required/>
@@ -42,7 +43,7 @@ function Adminlogin() {
                 
                    <TextField id="standard-basic" type='password' value={password} onChange={(e)=>{setPassword(e.target.value)}} sx={{width:"80%",margin:"10px  0 10px 0"}} label="Password" variant="standard" required/>
                 
-               {error && <div>{error}</div>}
+                   {error && <div>{error}</div>}
                     
                  <Button type='submit' sx={{  width: "80%"}} onClick={submitAdminLogin} size='large' variant="outlined"className='btn-admin' color='secondary' >Login</Button>
 
